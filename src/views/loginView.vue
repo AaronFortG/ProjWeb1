a8fe8baa35f13ae7cc21b545a0316fbfc4d85617
marcosruiz-flores <marcos.ruizflores@students.salle.url.edu>
2023-11-05 12:41:02 +0100

    vista de login ready

--- src/views/loginView.vue
+++ src/views/loginView.vue
@@ -1,11 +1,67 @@
-<script setup>
-
-</script>
-
+import {RouterLink} from "vue-router";
 <template>
-  
+  <div class="login-container">
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <p id="Log in">Log in</p>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="text" id="email" v-model="email" @input="validateEmail" required />
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+
+      <button id="login-button" type="submit">Login</button>
+      <button id="register-button" @click="register">Register</button>
+    </form>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    /**
+     * Válidamos el email del usuario que almenos contenga un @
+     */
+    validateEmail() {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(this.email)) console.log("Email format is not valid!.");
+    },
+    login() {
+      // Método para procesar el inicio de sesión
+      // Puedes agregar lógica para autenticar al usuario aquí
+      console.log("Iniciando sesión...");
+      console.log("Email:", this.email);
+      console.log("Password:", this.password);
+    },
+    register() {
+      // Método para redirigir al usuario a la página de registro
+      // Puedes implementar la lógica de redirección a la página de registro aquí
+      console.log("Redirigiendo al formulario de registro...");
+    },
+  },
+};
+</script>
+
+
 <style scoped>
+// Estilos CSS
+.login-container {
+  text-align: center;
+  margin: 20px;
+}
+
+.form-group {
+  margin: 10px;
+}
 
 </style>
