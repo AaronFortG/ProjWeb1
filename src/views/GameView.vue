b848c7cecaa3f8829175b568a222473c281b0357
Gemma <gemma.yebra321@gmail.com>
2023-11-10 20:11:37 +0100

    S'ha fet que els botons als clicar-los la imatge canvia

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -2,6 +2,58 @@
 import '../assets/game.css';
 </script>
 
+<script>
+  function setButtonState(buttonId, isPressed) {
+    const button = document.getElementById(buttonId);
+    if (isPressed) {
+      button.style.backgroundColor = '#d0d0d0'; // Change color when pressed
+    } else {
+      button.style.backgroundColor = ''; // Restore color when not pressed
+    }
+  }
+
+  document.addEventListener('keydown', function(event) {
+    switch (event.keyCode) {
+      case 37: // Arrow Left
+        setButtonState('arrowLeftButton', true);
+        break;
+      case 38: // Arrow Up
+        setButtonState('arrowUpButton', true);
+        break;
+      case 39: // Arrow Right
+        setButtonState('arrowRightButton', true);
+        break;
+      case 40: // Arrow Down
+        setButtonState('arrowDownButton', true);
+        break;
+      case 32: // Key Space
+        setButtonState('spaceKeyButton', true);
+        break;
+    }
+  });
+
+  document.addEventListener('keyup', function(event) {
+    switch (event.keyCode) {
+      case 37: // Arrow Left
+        setButtonState('arrowLeftButton', false);
+        break;
+      case 38: // Arrow Up
+        setButtonState('arrowUpButton', false);
+        break;
+      case 39: // Arrow Right
+        setButtonState('arrowRightButton', false);
+        break;
+      case 40: // Arrow Down
+        setButtonState('arrowDownButton', false);
+        break;
+      case 32: // Key Space
+        setButtonState('spaceKeyButton', false);
+        break;
+    }
+  });
+</script>
+
+
 <template>
 
   <div class="game_container">
@@ -211,20 +263,20 @@ import '../assets/game.css';
       </section>
 
       <section class="keys_container">
-        <button class="arrow-left">
+        <button class="arrow-left" id="arrowLeftButton">
           <img src="../assets/images/game/arrow-left.png" alt="arrow-left" width="30">
         </button>
-        <button class="arrow-up">
+        <button class="arrow-up" id="arrowUpButton">
           <img src="../assets/images/game/arrow-up.png" alt="arrow-up" width="30">
         </button>
-        <button class="arrow-down">
+        <button class="arrow-down" id="arrowDownButton">
           <img src="../assets/images/game/arrow-down.png" alt="arrow-down" width="30">
         </button>
-        <button class="arrow-right">
+        <button class="arrow-right" id="arrowRightButton">
           <img src="../assets/images/game/arrow-right.png" alt="arrow-right" width="30">
         </button>
 
-        <button class="space-key">
+        <button class="space-key" id="spaceKeyButton">
           <img src="../assets/images/game/space-key.png" alt="space-key" width="30">
         </button>
       </section>
