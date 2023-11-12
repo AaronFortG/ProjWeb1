9f34c62dfa5bb34bb24e9d9e1385c2d223212f88
Aaron Fort Garcia <aaron.fort@students.salle.url.edu>
2023-11-12 18:22:27 +0100

    · Mida del h2 canviat. · Els botons s'han canviat per router links (acceptats pel validator). · Els botons del popup s'han canviat per paràgrafs (acceptat pel validator). · Vista acabada, validada.

--- src/views/JoinGameView.vue
+++ src/views/JoinGameView.vue
@@ -32,75 +32,73 @@ export default {
 
 <template>
   <header class="header-buttons-container">
-    <RouterLink to="/home">
-      <button class="red_button" id="back-button">Back</button>
-    </RouterLink>
+    <router-link to="/home" class="red_button" id="back-button">Back</router-link>
 
-    <RouterLink to="/filter-arenas">
-      <button class="main-button" id="filter-button">Filter Arenas</button>
-    </RouterLink>
+    <router-link to="/filter-arenas" class="main-button" id="filter-button">Filter Arenas</router-link>
   </header>
 
-  <h1 class="title">Join an arena</h1>
-  <p id="arena-join-description">Right click an arena to join it.</p>
-
   <section>
-    <article class="arena" @click="showPopUpMethod">
-      <div class="arena-info">
-        <h2>Arena #1 - Date: </h2>
-        <h2>20/10/2023</h2>
-      </div>
-      <div class="arena-size">
-        <p>Grid size: </p>
-        <p>2x2</p>
-      </div>
-    </article>
-    <article class="arena" @click="showPopUpMethod">
-      <div class="arena-info">
-        <h2>Arena #2 - Date: </h2>
-        <h2>31/10/2023</h2>
-      </div>
-      <div class="arena-size">
-        <p>Grid size: </p>
-        <p>3x3</p>
-      </div>
-    </article>
-    <article class="arena" @click="showPopUpMethod">
-      <div class="arena-info">
-        <h2>Arena #3 - Date: </h2>
-        <h2>05/11/2023</h2>
-      </div>
-      <div class="arena-size">
-        <p>Grid size: </p>
-        <p>5x5</p>
-      </div>
-    </article>
-    <article class="arena" @click="showPopUpMethod">
-      <div class="arena-info">
-        <h2>Arena #4 - Date: </h2>
-        <h2>09/11/2023</h2>
-      </div>
-      <div class="arena-size">
-        <p>Grid size: </p>
-        <p>10x10</p>
-      </div>
-    </article>
-    <article class="arena" @click="showPopUpMethod">
-      <div class="arena-info">
-        <h2>Arena #5 - Date: </h2>
-        <h2>11/11/2023</h2>
-      </div>
-      <div class="arena-size">
-        <p>Grid size: </p>
-        <p>8x8</p>
-      </div>
-    </article>
+    <h1 class="title">Join an arena</h1>
+    <p id="arena-join-description">Right click an arena to join it.</p>
+
+    <div id="available-arenas">
+      <article class="arena" @click="showPopUpMethod">
+        <div class="arena-info">
+          <h2>Arena #1 - Date: </h2>
+          <h2>20/10/2023</h2>
+        </div>
+        <div class="arena-size">
+          <p>Grid size: </p>
+          <p>2x2</p>
+        </div>
+      </article>
+      <article class="arena" @click="showPopUpMethod">
+        <div class="arena-info">
+          <h2>Arena #2 - Date: </h2>
+          <h2>31/10/2023</h2>
+        </div>
+        <div class="arena-size">
+          <p>Grid size: </p>
+          <p>3x3</p>
+        </div>
+      </article>
+      <article class="arena" @click="showPopUpMethod">
+        <div class="arena-info">
+          <h2>Arena #3 - Date: </h2>
+          <h2>05/11/2023</h2>
+        </div>
+        <div class="arena-size">
+          <p>Grid size: </p>
+          <p>5x5</p>
+        </div>
+      </article>
+      <article class="arena" @click="showPopUpMethod">
+        <div class="arena-info">
+          <h2>Arena #4 - Date: </h2>
+          <h2>09/11/2023</h2>
+        </div>
+        <div class="arena-size">
+          <p>Grid size: </p>
+          <p>10x10</p>
+        </div>
+      </article>
+      <article class="arena" @click="showPopUpMethod">
+        <div class="arena-info">
+          <h2>Arena #5 - Date: </h2>
+          <h2>11/11/2023</h2>
+        </div>
+        <div class="arena-size">
+          <p>Grid size: </p>
+          <p>8x8</p>
+        </div>
+      </article>
+    </div>
   </section>
 
   <div id="popUp" class="popUp" v-show="showPopUp">
-    <p><b>Are you sure you want to join the game?</b></p>
-    <button @click="handleYesClick">Join arena</button>
-    <button @click="handleNoClick">Cancel</button>
+    <p class="popUp-question"><b>Are you sure you want to join the game?</b></p>
+    <p @click="handleYesClick">Join arena</p>
+    <p @click="handleNoClick">Cancel</p>
   </div>
 </template>
 
@@ -108,10 +106,11 @@ export default {
   .header-buttons-container {
     display: flex;
     justify-content: space-between;
-    padding: 2rem;
+    margin: 2rem;
     position: sticky;
     background-color: #2F2F2F;
     top: 0;
+    min-height: 3rem;
   }
 
   h1.title {
@@ -119,77 +118,58 @@ export default {
     margin-bottom: 2rem;
   }
 
-  button#filter-button {
+  #filter-button {
     margin-bottom: 0;
+    width: auto;
+    padding: 1rem;
   }
 
   section {
     margin: 2rem;
   }
 
-  p#arena-join-description {
+  #arena-join-description {
     color: white;
-    margin-bottom: 1rem;
-    margin-left: 2rem;
+    margin-bottom: 2rem;
   }
 
-  section article {
+  h2 {
+    font-size: 1rem;
+  }
+
+  section div article {
     background-color: #181414;
     padding: 1.5rem;
     margin-bottom: 2rem;
   }
 
-  section article p,
-  section article h2 {
+  section div article p,
+  section div article h2 {
     color: white;
   }
 
-  section article div.arena-info {
+  section div article div.arena-info {
     font-weight: bold;
     margin-bottom: 1rem;
   }
 
-  section article div.arena-info h2,
-  section article div.arena-size p {
+  section div article div.arena-info h2,
+  section div article div.arena-size p {
     display: inline;
   }
 
-  section article:hover {
+  section div article:hover {
     outline: white 1px solid;
   }
 
-  .popUp {
-    display: flex;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: white;
-    border: #cccccc 1px solid;
-    padding: 1rem;
-    width: 20rem;
-    z-index: 999; /* Per posar-lo per sobre dels elements */
-  }
-
-  .popUp *:nth-child(2) {
-    margin-bottom: 1rem;
-  }
-
-  .popUp button:hover {
-    font-weight: bold;
-  }
-
   @media (min-width: 1000px) {
-    section {
+    #available-arenas {
       display: flex;
       flex-wrap: wrap;
       column-gap: 2rem;
     }
 
-    section article.arena {
+    section div article.arena {
       flex: 1;
       max-width: 21.5rem;
       min-width: 21.5rem;
