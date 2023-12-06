15e27692fa1138945e1222288b2fe6bac11bc035
Gemma <gemma.yebra321@gmail.com>
2023-12-06 13:46:33 +0100

    S'han afegit varies coses.

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  props: {
+    placeHolder: String,
+    type: String
+  }
+}
+</script>
+
+<template>
+
+  <input
+      :type="this.type"
+      required
+      :placeholder="this.placeHolder"
+      @input="$emit('data', $event.target.value)"
+  />
+
+</template>
+
+<style scoped>
+
+  input {
+    width: 100%;
+    padding: 10px;
+    border: inset 1px  #ccc;
+    border-radius: 0;
+    background-color: #4e4a4e;
+    color: #fff;
+  }
+  input:focus {
+    outline: none;
+    box-shadow: inset 0 0 5px black;
+    border: 1px solid black;
+  }
+  ::placeholder {
+    color: #b2b2b2;
+  }
+</style>
